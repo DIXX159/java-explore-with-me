cdb873435b969d4181e639c9169df1874261df5a
Dixx <dixx@list.ru>
2023-04-27 18:26:05 +0700

    Этап 1 на проверку

--- stats-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/HitRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/HitRepository.java
@@ -14,24 +14,28 @@ public interface HitRepository extends JpaRepository<Hit, Integer> {
     @Query("select new ru.practicum.explorewithme.stats.model.Stats(h.app, h.uri, count(h.ip)) from Hit as h " +
             "where h.uri in ?1 and " +
             "h.timestamp between ?2 and ?3 " +
-            "group by h.app, h.uri ")
+            "group by h.app, h.uri " +
+            "order by count(h.ip) desc")
     List<Stats> findAllByUriContainsAndTimestampBetween(String[] uris, LocalDateTime start, LocalDateTime end);
 
     @Query("select new ru.practicum.explorewithme.stats.model.Stats(h.app, h.uri, count(h.ip)) from Hit as h " +
             "where " +
             "h.timestamp between ?1 and ?2 " +
-            "group by h.app, h.uri ")
+            "group by h.app, h.uri " +
+            "order by count(h.ip) desc")
     List<Stats> findAllByTimestampBetween(LocalDateTime start, LocalDateTime end);
 
-    @Query("select new ru.practicum.explorewithme.stats.model.Stats(h.app, h.uri, count(DISTINCT h.ip)) from Hit as h " +
+    @Query("select new ru.practicum.explorewithme.stats.model.Stats(h.app, h.uri, count(distinct h.ip)) from Hit as h " +
             "where " +
             "h.timestamp between ?1 and ?2 " +
-            "group by h.app, h.uri")
+            "group by h.app, h.uri " +
+            "order by count(distinct h.ip) desc")
     List<Stats> findUniqueStatsByTimestampBetween(LocalDateTime start, LocalDateTime end);
 
-    @Query("select new ru.practicum.explorewithme.stats.model.Stats(h.app, h.uri, count(DISTINCT h.ip)) from Hit as h " +
+    @Query("select new ru.practicum.explorewithme.stats.model.Stats(h.app, h.uri, count(distinct h.ip)) from Hit as h " +
             "where h.uri in ?1 and " +
             "h.timestamp between ?2 and ?3 " +
-            "group by h.app, h.uri")
+            "group by h.app, h.uri " +
+            "order by count(distinct h.ip) desc")
     List<Stats> findUniqueStatsByUriContainsAndTimestampBetween(String[] uris, LocalDateTime start, LocalDateTime end);
 }
