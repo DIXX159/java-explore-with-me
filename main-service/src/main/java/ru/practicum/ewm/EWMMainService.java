5fd134d4248794c1b4a1716fc4100ddd70000613
Dixx <dixx@list.ru>
2023-06-22 02:34:14 +0700

    Этап 2 на проверку

--- main-service/src/main/java/ru/practicum/ewm/EWMMainService.java
+++ main-service/src/main/java/ru/practicum/ewm/EWMMainService.java
@@ -2,10 +2,8 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath*:application-context.xml"})
 public class EWMMainService {
     public static void main(String[] args) {
         SpringApplication.run(EWMMainService.class, args);
