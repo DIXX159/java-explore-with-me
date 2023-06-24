d2ae50f0ea10d5b2eb8ddbdc32ceb56a31dee610
Dixx <dixx@list.ru>
2023-06-24 12:16:22 +0700

    Этап 2 на проверку

--- main-service/src/main/java/ru/practicum/ewm/client/StatsServiceClient.java
+++ main-service/src/main/java/ru/practicum/ewm/client/StatsServiceClient.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
@@ -13,7 +12,6 @@ import ru.practicum.dto.EndpointHit;
 import java.util.Map;
 
 @Service
-@PropertySource(value = "classpath:")
 public class StatsServiceClient extends Client {
     private static final String API_PREFIX = "";
 
